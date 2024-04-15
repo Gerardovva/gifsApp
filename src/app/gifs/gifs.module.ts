97c5d012168654effe6d08c6b4e4bbc355d4d8f0
Gerardo vasquez <gvsvasquez90@gmail.com>
2024-04-15 17:02:12 -0600

    actualizacion

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.components';
 import { CardListComponent } from './components/card-list/card-list.component';
@@ -14,7 +17,8 @@ import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
     GifsCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],exports:[
     HomePageComponent,
   ]
